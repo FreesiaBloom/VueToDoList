4eec420a3f4b66fb173ef57d89a0fee9a588c7d0
jasinaga <agata.jasinska@tieto.com>
2019-11-05 14:55:36 +0100

    adding todo

--- crash_todolist/src/App.vue
+++ crash_todolist/src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <div id="app">
+    <Header />
+    <AddTodo v-on:add-todo="addTodo"/>
     <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
   </div>
 </template>
 
 <script>
 import Todos from './components/Todos';
-
+import Header from './components/layout/Header'
+import AddTodo from './components/AddTodo'
 
 export default {
   name: 'app',
   components: {
+    Header,
     Todos,
+    AddTodo
   },
   data() {
     return {
@@ -37,6 +42,9 @@ export default {
   methods: {
     deleteTodo(id) {
       this.todos = this.todos.filter(todo => todo.id !== id);
+    },
+    AddTodo(newTodo) {
+      this.todos = [...this.todos, newTodo];
     }
   }
 }
@@ -52,5 +60,18 @@ export default {
 body {
   font-family: 'Lato', sans-serif;
   line-height: 1.4;
+}
+
+.btn {
+  display: inline-block;
+  border: none;
+  background: #555;
+  color: #fff;
+  padding: 7px 20px;
+  cursor: pointer;
+}
+
+.btn:hover {
+  background: #666;
 }
 </style>
